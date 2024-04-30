403b4e35d574a2c511c02531cb13a6fe6ff2842d
RajinikarBiram <rajinikar1445@gmail.com>
2024-04-30 15:52:59 -0700

    functions

--- mysql.sh
+++ mysql.sh
@@ -2,13 +2,13 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." f1)
+SCRIPT_NAME=$(echo $0 | cut -d "."-f1)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
 N="\e[0m"
 Y="\e[33m"
-echo " Please enter DB password:"
+echo "Please enter DB password:"
 read -s mysql_root_password
 
 VALIDATE(){
@@ -41,7 +41,7 @@ VALIDATE $? "Starting MYSQL server"
 # VALIDATE $? "Setting up root password"
 
 # Below code will be used to idempotent nature
-mysql -h db.rajinikar.cloud -uroot -p${mysql_root_password} -e 'show databases'; &>>$LOGFILE
+mysql -h db.rajinikar.cloud -uroot -p${mysql_root_password} -e 'show databases;' &>>$LOGFILE
 if [ $? -ne 0 ]
 then
     mysql_secure_installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
