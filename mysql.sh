bd498fb18468c132b0a804a2b3b22e1f3ba4c4be
RajinikarBiram <rajinikar1445@gmail.com>
2024-05-02 11:03:43 -0700

    functions

--- mysql.sh
+++ mysql.sh
@@ -44,5 +44,11 @@ VALIDATE $? "Starting MYSQL server"
 #below code will be used for idempotent nature
 mysql -h db.rajinikar.cloud -uroot -p{mysql_root_password} -e 'show databases;' &>>$LOGFILE
 
+if [ $? -ne 0]
+then
+   mysql-secure-installation --set-root-pass ${mysql_root_password} &>>$LOGFILE
+else 
+   echo -e " MySQL root password is already setup...$Y SKIPPING $N"
+fi
 
 
